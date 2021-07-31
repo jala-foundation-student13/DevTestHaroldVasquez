c0aa4fd9a961cc36cf40806eac48c6ced872cc24
HaroldVasquez <haroldvr.206@gmail.com>
2021-07-31 09:43:49 -0400

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  const url = 'https://randomuser.me/api/?page=2&results=10&seed=abc'
+  
+  const [apiList, setApiList] = useState()
+  
+  const apiCall = async () =>{
+    const response = await fetch(url, {
+      method : 'GET'
+    })
+    const responseJson = response.json();
+    setApiList(responseJson);
+  }
+
+  
+
+  useEffect(() => {
+    apiCall();
+  }, [])
+
+  // console.log(apiList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      if(!apiList){
+        'Cargando...'
+      }
+      else{
+        apiList.map( (element, index) =>{
+          return <li key = {index}>
+            {element.gender}
+            
+          </li>
+        })
+      }
     </div>
   );
 }
